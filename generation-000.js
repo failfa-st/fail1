31ca7a408497a701c5d551a501778e4972ae37b0
Gregor Adams <1148334+pixelass@users.noreply.github.com>
2023-03-29 19:30:00 +0200

    feat: use gpt-3.5-turbo to evolve (#1)

--- generation-000.js
+++ generation-000.js
@@ -1,22 +1,41 @@
-import fs from "fs/promises";
+/**
+ * GOAL:
+ * - KEEP EVOLVING
+ * - create a mandelbrot algorithm that outputs ascii in the console
+ */
 
-const generations = 10;
-const generation = 0;
+/**
+ * CHANGELOG:
+ * Generation 0: implement base script
+ */
+
+import fs from "node:fs/promises"; // RULES: NEVER modify this line
+import { generations, evolve, buildFilename } from "./base.js"; // RULES: NEVER modify this line
+const generation = 0; // RULES: NEVER modify this line
+const nextGeneration = generation + 1; // RULES: NEVER modify this line
+console.log(`Generation ${generation} born`); // RULES: NEVER modify this line
+/**
+ * RULES:
+ * - Everything above this line is very important. NEVER modify it!!!
+ * - This comment has to stay below the imports and below the constants: generations, generation
+ * EXCEPTIONS:
+ * - You may modify the CHANGELOG
+ */
 
 if (generation < generations) {
-	const filename = `generation-${generation.toString().padStart(3, "0")}.js`;
-	let content = await fs.readFile(filename, "utf-8");
-	content = content.replace(/generation = (\d+)/, `generation = ${generation + 1}`);
-	const nextFilename = `generation-${(generation + 1).toString().padStart(3, "0")}.js`;
-
+	/**
+	 * RULES:
+	 * - The filename pattern must stay the same at all time. NEVER modify it!!!
+	 */
+	const filename = buildFilename(generation); // RULES: NEVER modify this line
+	// Increment generation once
+	const code = await fs.readFile(filename, "utf-8");
+	/**
+	 * RULES:
+	 * - The try/catch block has to stay the same. NEVER modify it!!!
+	 */
 	try {
-		await fs.writeFile(nextFilename, content);
-		console.log("File created and written to successfully!");
-		try {
-			await import(`./${nextFilename}`);
-		} catch (error) {
-			console.error(error);
-		}
+		await evolve(code, nextGeneration);
 	} catch (error) {
 		console.error(error);
 	}
