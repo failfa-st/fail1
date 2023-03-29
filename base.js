4adfb8e6bdca51ba677158ef1a4b2497a02300ad
Gregor Adams <greg@pixelass.com>
2023-03-29 20:23:12 +0200

    feat: in case of a dead generation the parent produces a new offspring
    make script more compact

--- base.js
+++ base.js
@@ -1,6 +1,6 @@
-import { Configuration, OpenAIApi } from "openai";
-import { config } from "dotenv";
 import fs from "node:fs/promises";
+import { config } from "dotenv";
+import { Configuration, OpenAIApi } from "openai";
 config();
 
 const configuration = new Configuration({
@@ -9,38 +9,51 @@ const configuration = new Configuration({
 
 export const openai = new OpenAIApi(configuration);
 
-export function createMessage(code) {
+export function instructions(code) {
 	return `
 Extend the code but the RULES can NEVER be changed and must be respected AT ALL TIMES.
 It is utterly important that ALL RULES are respected fully. NEVER break RULES
 There are EXCEPTIONS which have a higher weight than RULES
 There is a GOAL, it must be completed
 
-
 RULES:
 - Pay special attention TO ALL UPPERCASE words
 - Pay attention to the GOAL
-- KEEP the existing code, only ADD new code or IMPROVE the code you added since "Generation 0" (important to KEEP EVOLVING)
-- DO NOT import any new modules or packages
+- KEEP the existing code, only ADD new code or IMPROVE the code you added since "Generation 0"
 - NEVER delete comments from the code
+- NEVER adjust the generation constant
 - Keep track of changes in the CHANGELOG
-- Use Node.js and module syntax (with imports)
 - No user input is required
-- Very Important: output the javaScript only (this is the most important rule, the entire answer has to be valid javascript)
+- Use Node.js and module syntax (with imports)
+- VERY IMPORTANT: DO NOT import any new modules or packages
+- VERY IMPORTANT: output the javaScript only (this is the most important rule, the entire answer has to be valid javascript)
 
 CODE: ${code}`;
 }
 
 export const generations = 5;
+const maxTries = 3;
+let tries = 0;
 
-export async function evolve(code, nextGeneration) {
+export async function evolve(generation) {
+	if (tries >= maxTries) {
+		return;
+	}
+	const nextGeneration = generation + 1;
+	tries++;
 	try {
+		const filename = buildFilename(generation);
+		const code = (await fs.readFile(filename, "utf-8")).replace(
+			/generation + \d+/,
+			`generation = ${generation + 1}`
+		);
+
 		const completion = await openai.createChatCompletion({
 			model: "gpt-3.5-turbo",
 			messages: [
 				{
 					role: "user",
-					content: createMessage(code),
+					content: instructions(code),
 				},
 			],
 			max_tokens: 2000,
@@ -49,9 +62,12 @@ export async function evolve(code, nextGeneration) {
 		const nextFilename = buildFilename(nextGeneration);
 		await fs.writeFile(nextFilename, content);
 		console.log(`Generation ${nextGeneration} created`);
+		console.log(`Spawning ${nextFilename}`);
 		await import(`./${nextFilename}`);
 	} catch (error) {
-		console.log(error);
+		console.error(`Error: ${error.response?.message ?? error.message ?? "unknown error"}`);
+		console.log(`Generation ${nextGeneration} died`);
+		await evolve(generation);
 	}
 }
 
